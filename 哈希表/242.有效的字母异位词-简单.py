a3e0ef770eb1a3b909fe4dcc23eba60e4d55c49a
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-08-13 18:24:54 +0900

    update

--- 哈希表/242.有效的字母异位词-简单.py
+++ 哈希表/242.有效的字母异位词-简单.py
@@ -12,3 +12,20 @@ class Solution:
         if len(s) != len(t):
             return False
         return Counter(s) == Counter(t)
+
+# 方法2: 哈希表 defaultdict
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_dict = defaultdict(int)
+        t_dict = defaultdict(int)
+
+        for i, j in zip(s, t):
+            s_dict[i] += 1
+            t_dict[j] += 1
+        
+        return s_dict == t_dict
--- 数组/0169.多数元素-简单.py
+++ 数组/0169.多数元素-简单.py
@@ -1,12 +1,13 @@
-import collections
-from typing import List
-
 """
+0169.多数元素
 https://leetcode.cn/problems/majority-element/
 给定一个大小为n的数组nums，返回其中的多数元素。
 多数元素是指在数组中出现次数大于⌊n/2⌋的元素。
 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
 """
+import collections
+from typing import List
+
 
 # 计数
 class Solution:
