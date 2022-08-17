0570928497a041a275d1a707f0748a9846f38527
A <57210715+Sihan-A@users.noreply.github.com>
2022-08-17 15:59:40 +0900

    Delete LeetCode-medium directory

--- LeetCode-medium/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++
@@ -1,8 +0,0 @@
-"""
-34.在排序数组中查找元素的第一个和最后一个位置
-https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/
-给你一个按照非递减顺序排列的整数数组nums，和一个目标值target。
-请你找出给定目标值在数组中的开始位置和结束位置。
-如果数组中不存在目标值target，返回[-1,-1]。
-你必须设计并实现时间复杂度为O(logn)的算法解决此问题。
-"""
