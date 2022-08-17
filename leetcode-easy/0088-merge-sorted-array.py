77b5612fa4ec9207825a3aada4a7bc664b7f10b5
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-08-17 15:17:01 +0900

    update

--- leetcode-easy/0088-merge-sorted-array.py
+++ leetcode-easy/0088-merge-sorted-array.py
@@ -1,7 +1,17 @@
+"""
+88.合并两个有序数组
+https://leetcode.cn/problems/merge-sorted-array/
+给你两个按非递减顺序排列的整数数组nums1和nums2，
+另有两个整数m和n，分别表示nums1和nums2中的元素数目。
+请你合并nums2到nums1中，使合并后的数组同样按非递减顺序排列。
+注意：最终，合并后数组不应由函数返回，而是存储在数组nums1中。
+为了应对这种情况，nums1的初始长度为m+n，其中前m个元素表示应合并的元素，
+后n个元素为0，应忽略。nums2的长度为n。
+
+https://leetcode.cn/problems/merge-sorted-array/solution/he-bing-liang-ge-you-xu-shu-zu-by-leetco-rrb0/
+"""
+
 from typing import List
-
-# https://leetcode.cn/problems/merge-sorted-array/solution/he-bing-liang-ge-you-xu-shu-zu-by-leetco-rrb0/
-
 
 # 方法1
 class Solution:
